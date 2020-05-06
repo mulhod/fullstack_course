518a231e5b3dc3e2e87c3c153da158dd58092144
Matthew Mulholland <mmulholland@ets.org>
2020-05-05 23:27:02 -0400

    part2.5: separate module

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,40 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({name}) => (
-  <>
-    <h1>{name}</h1>
-  </>
-)
-
-const Part = ({part}) => (
-  <>
-    {part.name} {part.exercises}
-    <br/><br/>
-  </>
-)
-
-const Content = ({parts}) => (
-  <>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </>
-)
-
-const Total = ({parts}) => (
-  <>
-    <b>total of {parts.map(part => part.exercises).reduce((prev, acc) => prev + acc)} exercises</b>
-  </>
-)
-
-const Course = ({course}) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  )
-}
+import Course from './components/Course';
 
 const App = () => {
   const courses = [
