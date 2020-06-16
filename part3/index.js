2461f8aca5db505665578b7a827a596fc60c48a1
Matthew Mulholland <mmulholland@ets.org>
2020-06-15 22:16:24 -0400

    part3.7: Phonebook backend step7

--- part3/index.js
+++ part3/index.js
@@ -1,20 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
 app.use(express.json())
-app.use(requestLogger)
+app.use(morgan('tiny'))
 
 let persons = [
   {
@@ -97,8 +87,6 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
-
-app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
