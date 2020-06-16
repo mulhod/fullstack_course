836360d20c686adec26553661ccd2099a89e0743
Matthew Mulholland <mmulholland@ets.org>
2020-06-15 22:13:06 -0400

    Use logger/unknown endpoint middleware

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(express.json())
+app.use(requestLogger)
 
 let persons = [
   {
@@ -84,6 +97,8 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
